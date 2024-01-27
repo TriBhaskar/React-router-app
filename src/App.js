1c9278868a3819198faa8c56f83c7c0bcf364199
TriBhaskar <panthribhaskar@gmail.com>
2024-01-27 21:10:18 +0530

    Add react-router-dom and configure router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import HomePage from "./pages/Home";
+
+const router = createBrowserRouter([{ path: "/", element: <HomePage /> }]);
 function App() {
-  return <div></div>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
