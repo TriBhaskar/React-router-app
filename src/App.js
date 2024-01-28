a3ede5e4ac58e7a690b5d6f8f9763342420a7561
TriBhaskar <panthribhaskar@gmail.com>
2024-01-28 21:40:23 +0530

    adding navigation with routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import HomePage from "./pages/Home";
 import ProductsPage from "./pages/Products";
+import RootPage from "./pages/Root";
 
 const router = createBrowserRouter([
-  { path: "/", element: <HomePage /> },
-  { path: "/products", element: <ProductsPage /> },
+  {
+    path: "/",
+    element: <RootPage />,
+    children: [
+      { path: "/", element: <HomePage /> },
+      { path: "/products", element: <ProductsPage /> },
+    ],
+  },
 ]);
 function App() {
   return <RouterProvider router={router} />;
---
+++ src/pages/Root.js
@@ -0,0 +1,13 @@
+import { Outlet } from "react-router-dom";
+import MainNavigation from "../components/MainNavigation";
+import classes from "./Root.module.css";
+export default function RootPage() {
+  return (
+    <>
+      <MainNavigation />
+      <main className={classes.content}>
+        <Outlet />
+      </main>
+    </>
+  );
+}
