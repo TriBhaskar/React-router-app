2d3a96040128bd8ab2b23440bd08ab24abb6c264
TriBhaskar <panthribhaskar@gmail.com>
2024-01-28 20:52:24 +0530

    Add Link component to Home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,15 @@
+import { Link } from "react-router-dom";
+
 function HomePage() {
-  return <h1>My Home Page</h1>;
+  return (
+    <>
+      <h1>My Home Page</h1>
+      <p>
+        Go to <Link to="/products">the list of products.</Link>{" "}
+        {/* Instead of <a> tag we use Link component. */}
+      </p>
+    </>
+  );
 }
 
 export default HomePage;
