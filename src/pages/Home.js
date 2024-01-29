49d96c3b7f6216cc77f803e202cfcbd2b3b17a14
TriBhaskar <panthribhaskar@gmail.com>
2024-01-29 21:35:09 +0530

    navigating programatically to other pages

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,18 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 function HomePage() {
+  const navigate = useNavigate();
   return (
     <>
       <h1>My Home Page</h1>
       <p>
         Go to <Link to="/products">the list of products.</Link>{" "}
         {/* Instead of <a> tag we use Link component. */}
+      </p>
+      <p>
+        <button onClick={() => navigate("/products")}>
+          Go to the list of products
+        </button>
       </p>
     </>
   );
