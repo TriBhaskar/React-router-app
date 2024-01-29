1b02fd936d4f1adde2877fda3825b2a366cb27c5
TriBhaskar <panthribhaskar@gmail.com>
2024-01-29 20:32:30 +0530

    Add ErrorPage component if invalid is entered

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import HomePage from "./pages/Home";
 import ProductsPage from "./pages/Products";
 import RootPage from "./pages/Root";
+import ErrorPage from "./pages/Error";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <RootPage />,
+    errorElement: <ErrorPage />,
     children: [
       { path: "/", element: <HomePage /> },
       { path: "/products", element: <ProductsPage /> },
--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,11 +1,12 @@
-import { Outlet } from "react-router-dom";
 import MainNavigation from "../components/MainNavigation";
-export default function RootPage() {
+
+export default function ErrorPage() {
   return (
     <>
       <MainNavigation />
       <main>
-        <Outlet />
+        <h1>An error occurred!</h1>
+        <p>Could not find this page</p>
       </main>
     </>
   );
