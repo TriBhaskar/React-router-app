eb9e340c436f9c45891bc5a687e3fdfd54783624
TriBhaskar <panthribhaskar@gmail.com>
2024-01-30 20:59:38 +0530

    Refactor ProductsPage to dynamically render product list

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,11 +1,26 @@
+import { Link } from "react-router-dom";
+
+const PRODUCTS = [
+  { id: "p1", title: "A Book", price: 6, description: "A great book!" },
+  { id: "p2", title: "A Carpet", price: 66, description: "Red and green." },
+  {
+    id: "p3",
+    title: "An Online Course",
+    price: 6,
+    description: "Learn to code!",
+  },
+];
+
 function ProductsPage() {
   return (
     <>
       <h1>The Products Page</h1>
       <ul>
-        <li>A Book</li>
-        <li>A Carpet</li>
-        <li>An Online Course</li>
+        {PRODUCTS.map((product) => (
+          <li key={product.id}>
+            <Link to={`/products/${product.id}`}>{product.title}</Link>
+          </li>
+        ))}
       </ul>
     </>
   );
